61478f382fcedceaa6774ffe1fbf9604105d6780
Jon Rich <jon@jonrich.co.uk>
2021-12-23 07:42:37 +0000

    Got MVP TIDY back up and working
    some tiny errors from me throwing the whole thing off...

--- led_on.py
+++ led_on.py
@@ -14,7 +14,7 @@ def parse_zone_configs(val):
 LED_PIN_BRIGHT = 13
 LED_PIN_WARM = 17
 
-LAMP_ON_START = parse_zone_configs(os.getenv("LAMP_ON_START","8:00"))
+LAMP_ON_START = parse_zone_configs(os.getenv("LAMP_ON_START","7:00"))
 LAMP_OFF_START = parse_zone_configs(os.getenv("LAMP_OFF_START","20:00"))
 
 LAMP_WARM_START = parse_zone_configs(os.getenv("LAMP_WARM_START","16:00"))
@@ -29,19 +29,23 @@ ledwarm = PWMLED(LED_PIN_WARM)
 
 #Define lamp state times
 print("Initialise lamp state times")
-on_zone = TimeOfDay(LAMP_ON_START, LAMP_OFF_START) # lamp on
-off_zone = TimeOfDay(LAMP_OFF_START, LAMP_ON_START) # lamp off
+on_zone = TimeOfDay(time(7), time(20)) # lamp on
+off_zone = TimeOfDay(time(20), time(7)) # lamp off
+#on_zone = TimeOfDay(LAMP_ON_START, LAMP_OFF_START) # lamp on
+#off_zone = TimeOfDay(LAMP_OFF_START, LAMP_ON_START) # lamp off
 
 #Define lamp temperature times
 print("Initialise lamp temperature times")
-bright_zone = TimeOfDay(LAMP_BRIGHT_START, LAMP_WARM_START) # bright white
-warm_zone = TimeOfDay(LAMP_WARM_START, LAMP_BRIGHT_START) # warm white
+bright_zone = TimeOfDay(time(9), time(16)) # bright white
+warm_zone = TimeOfDay(time(16), time(9)) # warm white
+#bright_zone = TimeOfDay(LAMP_BRIGHT_START, LAMP_WARM_START) # bright white
+#warm_zone = TimeOfDay(LAMP_WARM_START, LAMP_BRIGHT_START) # warm white
 
 #Define fade time from colour temperatures and on/off fade
 print("Initialise lamp fade duration")
 FADE_DURATION = int(os.getenv("FADE_DURATION","30")) #seconds
 
-print("Parameters defined")
+print("Instances defined")
 
 ###DEFINE FUNCTIONS
 
@@ -84,26 +88,56 @@ def fade_bright_to_warm(duration):
     ledwarm.pulse(duration,0,1,False)
     ledwarm.value = 1
 
+#Temperature zone settings
+def fade_bright_stay_on():
+    print("Function activated --- Fade on brightFade on bright")
+    fade_on_bright(FADE_DURATION)
+    bright_on()
+
+def fade_warm_stay_on():
+    print("Function activated --- Fade on warm")
+    fade_on_warm(FADE_DURATION)
+    warm_on()
+
+def warm_fade_stay_off():
+    print("Function activated --- Fade off warm")
+    fade_off_warm(FADE_DURATION)
+    lamp_off()
+
+def fade_warm_to_bright_stay_on():
+    print("Function activated --- Fade warm to bright")
+    warm_on()
+    fade_warm_to_bright(FADE_DURATION)
+    bright_on()
+
+def fade_bright_to_warm_stay_on():
+    print("Function activated --- Fade bright to warm")
+    bright_on()
+    fade_bright_to_warm(FADE_DURATION)
+    warm_on()
+
 def test_cycle():
     print("-Fade on night")
-    fade_on_warm(FADE_DURATION)
+    fade_warm_stay_on()
     sleep(5)
     print("--Sunrise start")
-    fade_warm_to_bright(FADE_DURATION)
+    fade_warm_to_bright_stay_on()
     sleep(5)
     print("---Daytime")
     bright_on()
     sleep(5)
     print("----Sunsset start")
-    fade_bright_to_warm(FADE_DURATION)
+    fade_bright_to_warm_stay_on()
     sleep(5)
     print("-----Nighttime")
     warm_on()
     sleep(5)
     print("------Turning off...")
-    fade_off_warm(FADE_DURATION)
+    warm_fade_stay_off()
     sleep(5)
     print("Test cycle complete")
+
+print("Functions defined")
 
 ###RUNNING SCRIPT
 
@@ -117,17 +151,17 @@ if not off_zone.is_active:
     # if day zone active, do x
     if bright_zone.is_active:
         print("manually setting Bright Zone")
-        fade_on_bright(FADE_DURATION)
+        fade_bright_stay_on()
     # if night zone active, do y
     else:
         print("manually setting Warm Zone")
-        fade_on_warm(FADE_DURATION)
+        fade_warm_stay_on()
 
 print("listening mode started")
 
-on_zone.when_activated = fade_on_warm(FADE_DURATION)
-warm_zone.when_activated = fade_bright_to_warm(FADE_DURATION)
-bright_zone.when_activated = fade_warm_to_bright(FADE_DURATION)
-off_zone.when_activated = fade_off_warm(FADE_DURATION)
+on_zone.when_activated = fade_warm_stay_on
+warm_zone.when_activated = fade_bright_to_warm_stay_on
+bright_zone.when_activated = fade_warm_to_bright_stay_on
+off_zone.when_activated = warm_fade_stay_off
 
 pause ()
